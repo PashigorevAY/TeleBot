0afb084bb6bef84e96b9ff59143c1cc21911ffa9
PashigorevAY <78847853+PashigorevAY@users.noreply.github.com>
2021-03-02 00:26:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_text_messages(message):
         try:
             receive = requests.get(API_URL.format(city=message.text))
             if receive.status_code == 200:
-                bot.send_message(message.from_user.id, receive.content)
+#                bot.send_message(message.from_user.id, receive.content)
                 bot.send_message(message.from_user.id, parse_receive(receive.content))
             else:
                 bot.send_message(message.from_user.id, 'Город не найден')
@@ -39,52 +39,3 @@ bot.polling()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#APIWheatherKEY = '2a34a0d26cbe1480d486f47f335436dc'
-#SberTestUser
-
-
-
-
-#URL = 'https://api.telegram.org/bot{token}/{method}'
-
-
-
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
